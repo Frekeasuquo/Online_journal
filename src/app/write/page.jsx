f309096e1606192a320041c50c2f6a19a4585b00
Frekeasuquo <asuquondifreke06@gmail.com>
2023-10-17 20:39:59 +0100

    db

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -12,7 +12,7 @@ const WritePage = () => {
     const [value, setValue] = useState("")
     return (
         <div className={styles.container}>    
-            <input type='text' placeholder='Title' />
+            <input type='text' placeholder='Title' className={styles.input}/>
             <div className={styles.editor}>
                 <button className={styles.button} onClick= {() => setOpen(!open)}>
                     <Image src="/plus.png" alt='' width={16} height={16} />
@@ -39,6 +39,7 @@ const WritePage = () => {
                     placeholder="Tell your story..."
                 />
             </div>
+            <button className={styles.publish}>Publish</button>
         </div>
     )
 }
