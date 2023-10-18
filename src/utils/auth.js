5b9bfaf9909595a4ce180ff4ec0bcb940c9a2bfb
Frekeasuquo <asuquondifreke06@gmail.com>
2023-10-18 21:01:11 +0100

    Working on the schema

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,12 +1,13 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { Prisma } from "@prisma/client"
+// import { Prisma } from "@prisma/client"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import prisma from "./connect"
 
 
 export const authOptions = {
-    adapter: PrismaAdapter(Prisma),
-    providers: [
+    adapter: PrismaAdapter(prisma),
+    providers: [  
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
