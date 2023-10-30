aa16bfb204f53d30414e4879e8372a7e4532db65
Frekeasuquo <asuquondifreke06@gmail.com>
2023-10-30 18:57:25 +0100

    updates

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,27 +2,27 @@ import Link from "next/link";
 import styles from "./card.module.css"
 import Image from "next/image";
 
-const Card = () => {
+const Card = ({ key, item}) => {
 
     return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
-            </div>
+        <div className={styles.container} key={key}>
+            {item.img && (
+                <div className={styles.imageContainer}>
+                    <Image src={item.img} alt="" fill className={styles.image}/>
+                </div>
+            )}
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>10.09.2023 - </span>
-                    <span className={styles.category}>CULTURE</span>
+                    <span className={styles.date}>{item.createdAt.substring(0, 10)} - </span>
+                    <span className={styles.category}>{item.catSlug}</span>
                 </div>
-                    <Link href="/">
-                        <h1>This is the title to my page, hope it is worth your time.</h1>
+                    <Link href={`/posts/${item.slug}`}>
+                        <h1>{item.title}</h1>
                     </Link>
                     <p className={styles.desc}>
-                        The stats says just on practising you will be perfect at it someday but I do not know when.
-                        The stats says just on practising you will be perfect at it someday but I do not know when.
-                        The stats says just on practising you will be perfect at it someday but I do not know when.
+                        {item.desc.substring(0, 60)}
                     </p>
-                    <Link href="/" className={styles.link}>Read More</Link>
+                    <Link href={`/posts/${item.slug}`} className={styles.link}>Read More</Link>
             </div>
         </div>
     )
